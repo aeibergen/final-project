0de5f779af6ad8103f3d2306b6842672bd243fb0
aeibergen <aeibergen@wisc.edu>
2017-04-18 11:51:10 -0500

    added leaflet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 //Creators: Andrea, Ben, and Sulong
 //Final Project
 //4-18-2017
+
+function createMap(){
+    //create the map
+    var map = L.map('mapid', {
+        center: [20, 0],
+        zoom: 2,
+        maxBounds:[
+            [70, 176],
+            [-48, -130]
+        ],
+    });
+};
