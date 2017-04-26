3362b61761aa91d36ba7783c3041d9f7bbf50d2b
Benjamin Segal <ben.segal7@gmail.com>
2017-04-26 15:30:59 -0500

    Hotspot Layer Complete w/o information

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ function createMap(){
     //create the map
     var map = L.map('mapid', {
         center: [20, 0],
-        zoom: 2
+        zoom: 2,
+        maxZoom:2
     });
 
     //add OSM base tilelayer
@@ -18,7 +19,9 @@ function createMap(){
 
  //call getData function
     getData(map);
-};
+
+
+var geojson;
 
 //function to retrieve the data and place it on the map
 function getData(map){
@@ -28,9 +31,106 @@ function getData(map){
         success: function(response){
 
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
+            geojson = L.geoJson(response, {
+
+
+
+            style: function (feature) {
+             if(feature.properties.TYPE === 'hotspot_area'){
+                    return {color: '#de2d26' ,
+                            weight: 2,
+                            stroke: 1};
+                  } else if(feature.properties.TYPE === 'outer_limit'){
+                    return { color: '#fc9272', 
+                             weight: 2,
+                             stroke: 0,
+                             fillOpacity: .5 };
+                  }
+            },
+
+            onEachFeature: function (feature, layer) {
+               var popupContent = "";
+                if (feature.properties) {
+                    //loop to add feature property names and values to html string
+                    
+                    popupContent += "<p>" + "Region" + ": " + feature.properties.NAME + "</p>";
+                    if (feature.properties.TYPE ==="hotspot_area"){
+                        popupContent += "<p>" + "Type: " + "Hotspot" + "</p>";
+                    } 
+                    
+                    if (feature.properties.TYPE ==="outer_limit"){
+                        popupContent += "<p>" + "Type: " + "Hotspot Outer Limit" + "</p>";
+                    } 
+
+                    layer.bindPopup(popupContent);
+                }; 
+
+               layer.on({
+                    mouseover: highlightFeature,
+                    mouseout: resetHighlight,
+                    click: zoomToFeature, 
+                    click: panelInfo
+                });
+
+               
+
+
+            }
+
+
+            }).addTo(map);
+
+            
+           var overlay  = {
+                "Hotspots" : geojson
+            };
+
+            L.control.layers(null, overlay).addTo(map);
+
+           
+
+            
+           
         }
     });
+};
+
+
+function panelInfo (e) {
+    var layer = e.target;
+    
+    $("#panel").html(layer.feature.properties.NAME);
+
+}
+
+
+function highlightFeature(e) {
+   var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        stroke: 1,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+};
+
+function resetHighlight(e) {
+    var layer = e.target;
+    geojson.resetStyle(e.target);
+
+    layer.closePopup();
+};
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+};
+
 };
 
 $(document).ready(createMap);
