53661e2797db5bf5b3f639959fd254e004578edd
Student <Student@shc.wisc.edu>
2017-04-26 15:54:31 -0500

    Resolved problems

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ function createMap(){
           [70, 176],
           [-48, -130]
         ],
-        // layers: [base, amphibians]
     });
 
     //create the layers to make the maps
@@ -29,58 +28,55 @@ function createMap(){
 
 
 var geojson;
-    //create option to have no biodiversity overlay 
-    var noneUrl = 'img/.jpg',
-    	noneBounds = [];
-   	var none = L.imageOverlay(noneUrl, noneBounds);
 
-   	var amphibianUrl = 'img/amphibian_richness_10km_all.png',
-    	amphibianBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var amphibians = L.imageOverlay(amphibianUrl, amphibianBounds);
+//function to retreve data
+function getData(map){
+  //load the data
+  $.ajax("data/hotspotData.geojson", {
+    dataType: "json",
+    success: function(response){
 
-            //create a Leaflet GeoJSON layer and add it to the map
-            geojson = L.geoJson(response, {
+    //create a Leaflet GeoJSON layer and add it to the map
+    geojson = L.geoJson(response, {
 
-
-
-            style: function (feature) {
-             if(feature.properties.TYPE === 'hotspot_area'){
-                    return {color: '#de2d26' ,
-                            weight: 2,
-                            stroke: 1};
-                  } else if(feature.properties.TYPE === 'outer_limit'){
-                    return { color: '#fc9272', 
-                             weight: 2,
-                             stroke: 0,
-                             fillOpacity: .5 };
-                  }
-            },
+        style: function (feature) {
+         if(feature.properties.TYPE === 'hotspot_area'){
+                return {color: '#de2d26' ,
+                        weight: 2,
+                        stroke: 1};
+              } else if(feature.properties.TYPE === 'outer_limit'){
+                return { color: '#fc9272',
+                         weight: 2,
+                         stroke: 0,
+                         fillOpacity: .5 };
+              }
+        },
 
             onEachFeature: function (feature, layer) {
                var popupContent = "";
                 if (feature.properties) {
                     //loop to add feature property names and values to html string
-                    
+
                     popupContent += "<p>" + "Region" + ": " + feature.properties.NAME + "</p>";
                     if (feature.properties.TYPE ==="hotspot_area"){
                         popupContent += "<p>" + "Type: " + "Hotspot" + "</p>";
-                    } 
-                    
+                    }
+
                     if (feature.properties.TYPE ==="outer_limit"){
                         popupContent += "<p>" + "Type: " + "Hotspot Outer Limit" + "</p>";
-                    } 
+                    }
 
                     layer.bindPopup(popupContent);
-                }; 
+                };
 
                layer.on({
                     mouseover: highlightFeature,
                     mouseout: resetHighlight,
-                    click: zoomToFeature, 
-                    click: panelInfo
+                    click: panelInfo,
+                    click: zoomToFeature
                 });
 
-               
+
 
 
             }
@@ -88,28 +84,71 @@ var geojson;
 
             }).addTo(map);
 
-            
-           var overlay  = {
+            //create option to have no biodiversity overlay
+            var noneUrl = 'img/.jpg',
+              noneBounds = [];
+            var none = L.imageOverlay(noneUrl, noneBounds);
+
+            var amphibianUrl = 'img/amphibian_richness_10km_all.png',
+              amphibianBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var amphibians = L.imageOverlay(amphibianUrl, amphibianBounds);
+
+            var caecilianUrl = 'img/caecilian_richness_10km.png',
+            	caecilianBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var caecilians = L.imageOverlay(caecilianUrl, caecilianBounds);
+
+            var anuraUrl = 'img/frog_richness_10km.png',
+            	anuraBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var anura = L.imageOverlay(anuraUrl, anuraBounds);
+
+            var caudataUrl = 'img/salamander_richness_10km.png',
+            	caudataBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var caudata = L.imageOverlay(caudataUrl, caudataBounds);
+
+            var psittaciformesUrl = 'img/psittaciformes_richness.png',
+                psittaciformesBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var psittaciformes = L.imageOverlay(psittaciformesUrl, psittaciformesBounds);
+
+            var passeriformesUrl = 'img/passeriformes_richness.png',
+            	passeriformesBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var passeriformes = L.imageOverlay(passeriformesUrl, passeriformesBounds)
+
+            var songbirdsUrl = 'img/songbirds_richness.png',
+            	songbirdsBounds = [[84.65, -220.9],[-62.24, 220.85]];
+            var songbirds = L.imageOverlay(songbirdsUrl, songbirdsBounds);
+
+
+            //category names for toggle layers
+            var animals = {
+            	"Overlays Off": none,
+            	"All Amphibians": amphibians,
+            	"Caecilian": caecilians,
+            	"Anura": anura,
+            	"Caudata": caudata,
+            	"Psittaciformes": psittaciformes,
+            	"Passeriformes": passeriformes,
+            	"Songbirds": songbirds
+            };
+
+            //
+            var overlay  = {
                 "Hotspots" : geojson
             };
 
-            L.control.layers(null, overlay).addTo(map);
+            //add heatmaps and hotspot to map
+            L.control.layers(animals, overlay).addTo(map);
 
-           
-
-            
-           
-        }
+          }
     });
 };
 
 
 function panelInfo (e) {
     var layer = e.target;
-    
+
     $("#panel").html(layer.feature.properties.NAME);
 
-}
+};
 
 
 function highlightFeature(e) {
@@ -140,64 +179,6 @@ function zoomToFeature(e) {
 };
 
 };
-    var caecilianUrl = 'img/caecilian_richness_10km.png',
-    	caecilianBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var caecilians = L.imageOverlay(caecilianUrl, caecilianBounds);
 
-    var anuraUrl = 'img/frog_richness_10km.png',
-    	anuraBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var anura = L.imageOverlay(anuraUrl, anuraBounds);
-
-    var caudataUrl = 'img/salamander_richness_10km.png',
-    	caudataBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var caudata = L.imageOverlay(caudataUrl, caudataBounds);
-
-    var psittaciformesUrl = 'img/psittaciformes_richness.png',
-        psittaciformesBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var psittaciformes = L.imageOverlay(psittaciformesUrl, psittaciformesBounds);
-
-    var passeriformesUrl = 'img/passeriformes_richness.png',
-    	passeriformesBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var passeriformes = L.imageOverlay(passeriformesUrl, passeriformesBounds)
-
-    var songbirdsUrl = 'img/songbirds_richness.png',
-    	songbirdsBounds = [[84.65, -220.9],[-62.24, 220.85]];
-    var songbirds = L.imageOverlay(songbirdsUrl, songbirdsBounds);
-
-
-    //category names for toggle layers
-    var animals = {
-    	"Overlays Off": none,
-    	"All Amphibians": amphibians,
-    	"Caecilian": caecilians,
-    	"Anura": anura,
-    	"Caudata": caudata,
-    	"Psittaciformes": psittaciformes,
-    	"Passeriformes": passeriformes,
-    	"Songbirds": songbirds
-    };
-
-    //  //call getData function
-    // var overlayMaps = {
-    // 	"Hotspots": getData(map)
-    // };
-
-    //add animals raster information to map
-    L.control.layers(animals).addTo(map);
-};
-
-
-// //function to retrieve the data and place it on the map
-// function getData(map){
-//     //load the data
-//     $.ajax("data/hotspotData.geojson", {
-//         dataType: "json",
-//         success: function(response){
-
-//             //create a Leaflet GeoJSON layer and add it to the map
-//             L.geoJson(response).addTo(map);
-//         }
-//     });
-// };
 
 $(document).ready(createMap);
